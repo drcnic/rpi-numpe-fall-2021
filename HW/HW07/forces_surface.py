d39da7b77716e3cd3c5d11e972e4fe0ffec0c9c0
drcnic <daricnic@gmail.com>
2021-10-29 15:31:24 -0400

    Update HW07

--- HW/HW07/forces_surface.py
+++ HW/HW07/forces_surface.py
@@ -22,11 +22,13 @@ def panel_midpoints(x, y):
     """
     
     num_panels = x.size - 1 
-    x_mid, y_mid = np.zeros(num_panels)
+    x_mid = np.zeros(num_panels)
+    y_mid = np.zeros(num_panels)
     
     for i in range(0, num_panels):
-        x_mid[i] = (x[i+1]+x[i])/2.
-        y_mid[i] = quad.midpoint(y[i:i+1], x[i+1]-x[i])
+        x_mid[i] = (x[i+1]+x[i])*0.5
+        y_mid[i] = (y[i+1]+y[i])*0.5
+        #y_mid[i] = quad.midpoint(y[i:i+1], x[i+1]-x[i])
                 
     return x_mid, y_mid
 
@@ -56,8 +58,10 @@ def panel_midforces(x, y, press):
     """
     
     num_panels = x.size - 1 
-    dfx, dfy = np.zeros(num_panels)
-    nx, ny   = np.ones(num_panels)
+    dfx = np.zeros(num_panels)
+    dfy = np.zeros(num_panels)
+    nx = np.ones(num_panels)
+    ny = np.ones(num_panels)
     
     # get the tip and tail locations of vectors normal to panels
     for i in range(0, num_panels):
@@ -67,16 +71,40 @@ def panel_midforces(x, y, press):
         slope = delta_y/delta_x
         
         inv_slope = -1.0/slope
-        scalar = 1.0/np.sqrt(1.0 + (inv_slope**2))  # we need to divide the vector by the square root of the dot product with itself
         
-        nx[i] *= scalar            # normal unit vector, x component
-        ny[i]  = inv_slope*scalar  # normal unit vector, y component
-        
-        #p_mag  = -0.5*(press[i] + press[i+1]) * np.sqrt(delta_x**2 + delta_y**2)
-        dfx[i] = -0.5*(press[i] + press[i+1]) * nx[i] * np.sqrt(delta_x**2 + delta_y**2)
-        dfy[i] = -0.5*(press[i] + press[i+1]) * ny[i] * np.sqrt(delta_x**2 + delta_y**2)
-        
+        length = np.sqrt(delta_x**2 + delta_y**2) # delta s
+        dfx[i] = -0.5*(press[i] + press[i+1]) * delta_y
+        dfy[i] = -0.5*(press[i] + press[i+1]) * -delta_x 
+
     return dfx, dfy
+
+def integrate_forces(dfx, dfy):
+    """
+    Numerically integrates panel forces using midpoint method
+
+    Parameters
+    ----------
+    dfx : 1D np array, floats
+        array of x component forces on each panel
+    dfy : 1D np array, floats
+        array of y component forces on each panel
+
+    Returns
+    -------
+    force_x : float
+        scalar net force in x direction
+    force_y : float
+        scalar net force in y direction
+
+    """
+
+
+    force_x = quad.midpoint(dfx, 1.0) 
+    force_y = quad.midpoint(dfy, 1.0) 
+    
+    return force_x, force_y
+     
+    
 
         
     
--- HW/HW07/hw07.py
+++ HW/HW07/hw07.py
@@ -1,4 +1,50 @@
 """hw07.py"""
 
 import matplotlib.pyplot as plt
+import numpy as np
+import forces_surface as fs
 
+# data organization
+data   = np.loadtxt("hydro_pressure.dat")
+theta  = data[:,0]
+x_data = data[:,1]
+y_data = data[:,2]
+press_data = data[:,3]
+
+
+
+
+# calculation
+x_mid, y_mid = fs.panel_midpoints(x_data, y_data)
+dfx, dfy = fs.panel_midforces(x_data, y_data, press_data)
+fx, fy = fs.integrate_forces(dfx, dfy)
+
+dfx_t = dfx/1.e6
+dfy_t = dfy/1.e6
+
+
+# plotting
+fig, ax = plt.subplots(1,1,figsize=(8,6))
+ax.axis('equal')
+
+plt.title("Hydrostatic Pressure in Ship Hull (niclad)", size=16)
+plt.xlabel("x", size=16)
+plt.ylabel("y", size=16)
+
+q = ax.quiver(x_mid, y_mid, dfx_t, dfy_t, color='r', units='xy', scale=1/24.)
+ax.quiverkey(q, X=0.7, Y=0.1, U=1/2., label="Force", labelpos = 'E')
+plt.plot(x_data, y_data, 'k-', linewidth=5)
+plt.plot(np.array([x_data[0], x_data[x_data.size-1]]), np.array([y_data[0], y_data[x_data.size-1]]), 'k-', linewidth=5) # connect ends of half circle
+plt.plot([-30, 30], [-10, -10], 'b-', linewidth=5)      # waterline
+
+# net force vector graphing
+mega = 1.e-6
+text_string = "Net force vector:\n("
+text_string += '{:6.4e}'.format(fx*mega)
+text_string += ", "
+text_string += '{:6.4e}'.format(fy*mega)
+text_string += ")"
+plt.text(-30,10, text_string)
+
+plt.savefig("hydrostatic_force.png")
+plt.show()
