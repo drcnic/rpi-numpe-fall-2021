186bab0a4748c3556adda043795ff2689ed54f40
drcnic <daricnic@gmail.com>
2021-10-29 15:33:26 -0400

    Add 10-29 lecture notes

---
+++ Lecture notes/lecture code 10-29.py
@@ -0,0 +1,34 @@
+""" 
+Encapsulation:
+    
+    Framework for having code that stores and manipulates data
+    in one place
+    
+    class Triangle():
+        __init__():
+            
+        calc_area():
+            
+
+Polymorphism:
+    
+    Framework, that makes us use calc_area for all 2d shapes
+    irrespective of the object.
+    
+    
+    Operator +
+    
+    a = 1 + 2
+    b = 22.3 + 44.0
+    st = "hello" + "world"
+    
+    
+    __add__()
+    
+    
+Inheritence:
+    
+    class class1():
+        pass
+    
+    class class2(class1):
